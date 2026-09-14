76df96619d3bc3982acea50124549c74dc3f6064
amit2647 <amitmahorkar798@gmail.com>
2026-09-14 20:44:06 +0530

    feat: add health check endpoint to lead service

--- src/app.js
+++ src/app.js
@@ -12,6 +12,13 @@ app.use(express.json());
 
 app.use(requestLogger);
 
+app.get("/health", (req, res) => {
+  res.status(200).json({
+    status: "ok",
+    service: "lead-service",
+  });
+});
+
 app.use(leadRoutes);
 
 module.exports = app;
